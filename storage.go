7f6fdcd537db0606d09b153d1fcc1372a9b0369f
Clauvio <2081215119@qq.com>
2024-06-09 17:44:31 +0800

    feat(Store): Delete and Has method

--- storage.go
+++ storage.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bytes"
-	"crypto/md5"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -12,7 +13,7 @@ import (
 )
 
 // CASPathTransformFunc 是将一个 key 通过散列转化为一个路径的函数
-func CASPathTransformFunc(key string) string {
+func CASPathTransformFunc(key string) PathKey {
 	hash := sha1.Sum([]byte(key)) // [20]byte -> []byte
 	hashStr := hex.EncodeToString(hash[:])
 
@@ -25,11 +26,31 @@ func CASPathTransformFunc(key string) string {
 		from, to := i*blockSize, (i+1)*blockSize
 		paths[i] = hashStr[from:to]
 	}
-	return strings.Join(paths, "/")
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		Filename: hashStr,
+	}
 }
 
 // PathTransformFunc 用于将一个key转换为一个路径
-type PathTransformFunc func(string) string
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	Filename string
+}
+
+func (p PathKey) FirstPathName() string {
+	paths := strings.Split(p.PathName, "/")
+	if len(paths) == 0 {
+		return ""
+	}
+	return paths[0]
+}
+
+func (p PathKey) FullPath() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.Filename)
+}
 
 // StoreOpts 保存了一个 Store 的配置
 type StoreOpts struct {
@@ -37,8 +58,11 @@ type StoreOpts struct {
 }
 
 // DefaultPathTransformFunc 是一个默认的 PathTransformFunc
-var DefaultPathTransformFunc = func(key string) string {
-	return key
+var DefaultPathTransformFunc = func(key string) PathKey {
+	return PathKey{
+		PathName: key,
+		Filename: key,
+	}
 }
 
 type Store struct {
@@ -46,37 +70,72 @@ type Store struct {
 }
 
 func NewStore(opt StoreOpts) *Store {
+	if opt.PathTransformFunc == nil {
+		opt.PathTransformFunc = DefaultPathTransformFunc
+	}
 	return &Store{
 		StoreOpts: opt,
 	}
 }
 
+func (s *Store) Has(key string) bool {
+	pathKey := s.PathTransformFunc(key)
+	_, err := os.Stat(pathKey.FullPath())
+	return !errors.Is(err, os.ErrNotExist)
+}
+
+// Delete 从磁盘上删除一个 key 对应的文件
+func (s *Store) Delete(key string) error {
+	pathKey := s.PathTransformFunc(key)
+
+	defer func() {
+		log.Printf("deleted [%s] from disk\n", pathKey.FullPath())
+	}()
+
+	// todo: 这种删除方式可能导致其他通过散列函数生成文件名前面的pad与想要删除的文件相同的文件被删除
+	return os.RemoveAll(pathKey.FirstPathName())
+}
+
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+
+	return buf, nil
+}
+
+func (s *Store) readStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	return os.Open(pathKey.FullPath())
+}
+
 // writeStream 将一个流写入到磁盘上
 func (s *Store) writeStream(key string, r io.Reader) error {
-	pathName := s.PathTransformFunc(key) // 通过传入的规则函数将 key 转化为路径
+	pathKey := s.PathTransformFunc(key) // 通过传入的规则函数将 key 转化为路径
 
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	if err := os.MkdirAll(pathKey.PathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	buf := new(bytes.Buffer)
-	io.Copy(buf, r)
+	fullPath := pathKey.FullPath()
+	fmt.Println(fullPath)
 
-	fileNameBytes := md5.Sum(buf.Bytes())
-	fileName := hex.EncodeToString(fileNameBytes[:])
-	pathAndFileName := pathName + "/" + fileName
-
-	f, err := os.Create(pathAndFileName)
+	f, err := os.Create(fullPath)
 	defer f.Close()
 	if err != nil {
 		return err
 	}
 
-	n, err := io.Copy(f, buf)
+	n, err := io.Copy(f, r)
 	if err != nil {
 		return err
 	}
-	log.Printf("written (%d bytes) to dist:%s\n", n, pathAndFileName)
+	log.Printf("written (%d bytes) to dist:%s\n", n, fullPath)
 
 	return nil
 }
--- storage_test.go
+++ storage_test.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"testing"
 )
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "momsbestrecipe"
-	pathName := CASPathTransformFunc(key)
+	pathKey := CASPathTransformFunc(key)
+	expectedOriginalKey := "9c6a869ebfa9c237594def249adac0b2c4582781"
 	expectedPathname := "9c6a8/69ebf/a9c23/7594d/ef249/adac0/b2c45/82781"
-	assert.Equal(t, pathName, expectedPathname)
+	assert.Equal(t, pathKey.PathName, expectedPathname)
+	assert.Equal(t, pathKey.Filename, expectedOriginalKey)
+}
+
+func TestStoreDeleteKey(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+
+	s := NewStore(opts)
+
+	key := "momsbestrecipe"
+	data := []byte("some jpg bytes")
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := s.Read(key); err == nil {
+		t.Error(err)
+	}
 }
 
 func TestNewStore(t *testing.T) {
@@ -20,9 +46,21 @@ func TestNewStore(t *testing.T) {
 
 	s := NewStore(opts)
 
-	data := bytes.NewReader([]byte("some jpg bytes"))
-	if err := s.writeStream("myspecialpicture", data); err != nil {
+	key := "momsbestrecipe"
+	data := []byte("some jpg bytes")
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
 	}
 
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := io.ReadAll(r)
+
+	fmt.Println(string(b))
+
+	assert.Equal(t, string(b), string(data))
+	assert.Equal(t, s.Has(key), true)
 }
