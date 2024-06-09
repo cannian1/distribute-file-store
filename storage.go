ff6a18fd2ef9afed5bdda85abaab25daeea35aa0
Clauvio <2081215119@qq.com>
2024-06-09 15:18:44 +0800

    feat: store file use custom func to change path

--- storage.go
+++ storage.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+// CASPathTransformFunc 是将一个 key 通过散列转化为一个路径的函数
+func CASPathTransformFunc(key string) string {
+	hash := sha1.Sum([]byte(key)) // [20]byte -> []byte
+	hashStr := hex.EncodeToString(hash[:])
+
+	blockSize := 5 // 转化后的路径名每层最大长度
+	sliceLen := len(hashStr) / 5
+
+	paths := make([]string, sliceLen)
+
+	for i := range sliceLen {
+		from, to := i*blockSize, (i+1)*blockSize
+		paths[i] = hashStr[from:to]
+	}
+	return strings.Join(paths, "/")
+}
 
 // PathTransformFunc 用于将一个key转换为一个路径
 type PathTransformFunc func(string) string
@@ -31,21 +53,26 @@ func NewStore(opt StoreOpts) *Store {
 
 // writeStream 将一个流写入到磁盘上
 func (s *Store) writeStream(key string, r io.Reader) error {
-	pathName := key
+	pathName := s.PathTransformFunc(key) // 通过传入的规则函数将 key 转化为路径
 
 	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	fileName := "some-file-name"
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r)
+
+	fileNameBytes := md5.Sum(buf.Bytes())
+	fileName := hex.EncodeToString(fileNameBytes[:])
 	pathAndFileName := pathName + "/" + fileName
 
 	f, err := os.Create(pathAndFileName)
+	defer f.Close()
 	if err != nil {
 		return err
 	}
 
-	n, err := io.Copy(f, r)
+	n, err := io.Copy(f, buf)
 	if err != nil {
 		return err
 	}
--- storage_test.go
+++ storage_test.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"bytes"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestrecipe"
+	pathName := CASPathTransformFunc(key)
+	expectedPathname := "9c6a8/69ebf/a9c23/7594d/ef249/adac0/b2c45/82781"
+	assert.Equal(t, pathName, expectedPathname)
+}
+
 func TestNewStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 
 	s := NewStore(opts)
